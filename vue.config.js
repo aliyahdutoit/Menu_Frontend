b57410e9578cd01f3a8525f70043e89ffda3972c
Aliyah Du Toit <aliyah.dutoit@gmail.com>
2024-03-25 15:23:29 +0200

    committing everything to a new repo

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Restaurant_Menu_Frontend/'
+      : '/'
+  }
+
+  console.log(publicPath);
+
+  
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  publicPath
+})
